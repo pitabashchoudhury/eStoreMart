d0f4509a8553813058c93322d4a213cf24ce75cb
pitabashchoudhury <pitabash98@gmail.com>
2026-01-02 11:41:10 +0530

    folder added

---
+++ eStore-Mart/src/main/java/org/ecom/estoremart/EStoreMartApplication.java
@@ -0,0 +1,13 @@
+package org.ecom.estoremart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EStoreMartApplication.class, args);
+    }
+
+}
---
+++ eStore-Mart/src/test/java/org/ecom/estoremart/EStoreMartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecom.estoremart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStoreMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
